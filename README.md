b1a193a337f87c5e3f2bd50285871a87cf75061e
Bruno Lorenzo <bruno.lorenzop@gmail.com>
2023-04-26 19:34:31 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# keychain-wrapper
+
+A description of this package.
---
+++ Tests/keychain-wrapperTests/keychain_wrapperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import keychain_wrapper
+
+final class keychain_wrapperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(keychain_wrapper().text, "Hello, World!")
+    }
+}
